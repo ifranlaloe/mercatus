7f2165b371ab0d51e7822f18f32a081359f83546
Ifran Laloe <ifran@live.nl>
2024-06-02 12:24:15 +0200

    Fixed refactor issue in data_handler

--- mercatus/algorithms/initializers/aggregator_initializer.py
+++ mercatus/algorithms/initializers/aggregator_initializer.py
@@ -1,6 +1,7 @@
 from data.day_aggregator import DayAggregator
 from data.week_aggregator import WeekAggregator
 from data.month_aggregator import MonthAggregator
+from data.minute_data_collector import MinuteDataCollector
 
 class AggregatorInitializer:
     def __init__(self, algorithm):
@@ -10,6 +11,7 @@ class AggregatorInitializer:
         self.algorithm.day_aggregator = DayAggregator(self.algorithm)
         self.algorithm.week_aggregator = WeekAggregator(self.algorithm)
         self.algorithm.month_aggregator = MonthAggregator(self.algorithm)
+        self.algorithm.minute_data_collector = MinuteDataCollector(self.algorithm, self.algorithm.spy)
 
         self.algorithm.Schedule.On(self.algorithm.DateRules.EveryDay(self.algorithm.spy), 
                                    self.algorithm.TimeRules.AfterMarketOpen(self.algorithm.spy, 1), 
